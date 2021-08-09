4717d4338af54338c2396c742575bde696f1368e
kimi <kimi0230@gmail.com>
2021-08-09 16:50:03 +0800

    format

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Switch, Redirect, useRouteMatch } from 'react-router-dom';
-import './App.css';
+import React, { Suspense } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+  useRouteMatch,
+} from "react-router-dom";
+import "./App.css";
 
-const Welcome = React.lazy(() => import('./view/welcome/welcome'));
-const Menu = React.lazy(() => import('./view/menu/menu'));
+const Welcome = React.lazy(() => import("./view/welcome/welcome"));
+const Menu = React.lazy(() => import("./view/menu/menu"));
 
 const App = () => (
   <BrowserRouter>
     {/* fix: [ERROR] A React component suspended while rendering, but no fallback UI was specified.
     https://stackoverflow.com/questions/54432861/a-react-component-suspended-while-rendering-but-no-fallback-ui-was-specified */}
     <Suspense fallback={<p>Loading...</p>}>
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/welcome" />
-      </Route>
-      <Route path="/welcome" exact>
-        <Welcome />
-      </Route>
-      <Route path="/menu">
-        <Menu />
-      </Route>
-    </Switch>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/welcome" />
+        </Route>
+        <Route path="/welcome" exact>
+          <Welcome />
+        </Route>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+      </Switch>
     </Suspense>
   </BrowserRouter>
 );
