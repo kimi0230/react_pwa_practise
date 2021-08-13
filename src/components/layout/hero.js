ebe48b029ebeaf2444b0fb75a0659da05a57b36c
kimi <kimi0230@gmail.com>
2021-08-13 17:59:18 +0800

    fix warming

--- src/components/layout/hero.js
+++ src/components/layout/hero.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import NetflixBackgroundImage from '../../asset/img/netflix-background.png';
-import NarcosLogo from '../../asset/img/narcos.logo.png';
+import React from "react";
+import NetflixBackgroundImage from "../../asset/img/netflix-background.png";
+import NarcosLogo from "../../asset/img/narcos.logo.png";
 
 const Hero = () => {
   return (
     <div id="hero" className="hero">
       <div className="content">
-        <img className="logo" src={NarcosLogo}/>
+        <img className="logo" alt="NarcosLogo" src={NarcosLogo} />
         <h2>Season 2 now available</h2>
         <p>
-          Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Doloremque id quam sapiente unde voluptatum alias vero debitis, magnam quis
-          quod.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
+          id quam sapiente unde voluptatum alias vero debitis, magnam quis quod.
         </p>
         <div className="button-wrapper">
-          <a href="#" className="button-el">Watch now</a>
-          <a href="#" className="button-el">My list</a>
+          <a href="/#" className="button-el">
+            Watch now
+          </a>
+          <a href="/#" className="button-el">
+            My list
+          </a>
         </div>
       </div>
-      <div className="overlay" style={{background: `url(${NetflixBackgroundImage})`}}/>
+      <div
+        className="overlay"
+        style={{ background: `url(${NetflixBackgroundImage})` }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
